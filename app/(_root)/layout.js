3b36f5ee548fe353e6d1b49217fc980e8f9bf783
tivereidoro <tivereidoro@yahoo.com>
2025-05-27 00:14:51 +0100

    Clean up: 2nd level root layout

--- app/(_root)/layout.js
+++ app/(_root)/layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// This is the 2nd level layout
+// 2nd level layout
 // for the root path
 export default function layout({ children }) {
   return <div className="relative font-satoshi">{children}</div>;
